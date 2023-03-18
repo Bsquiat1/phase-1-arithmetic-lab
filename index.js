191c9c98f0d368c1764959785e5c21aa6e8faff4
Denis <denis.were@student.moringaschool.com>
2023-03-18 22:45:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2
+let multiply = num1*num2;
+multiply;
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 100) + 1; // generates a random integer between 1 and 100
+
+let mod = num3 % num4;
+let num3 = 10
+let num4 = 6
+mod = 10 % 6;
+mod = 4;
+
+let numbers = [10, 5, 20, 15, 8];
+let max_num = Math.max(...numbers);
+let max = max_num;
